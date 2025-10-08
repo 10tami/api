205b6d7cb15295d396de65bba86744b3b94924a7
10tami <melinao10tami@gmail.com>
2025-10-08 18:34:23 -0300

    movi App.css a src

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState , useEffect} from 'react'
 import './App.css'
-
+import './App.css'
 function App() {
   const [data, setData] = useState([])
   const [abierto, setAbierto] = useState (true)
