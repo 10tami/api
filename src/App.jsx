d046d052dee02fc54e335f26bd5bd01a96662157
10tami <melinao10tami@gmail.com>
2025-10-07 21:19:21 -0300

    añadi un api de perritos

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([])
+  const [abierto, setAbierto] = useState (true)
+  const [cargando, setCargando] = useState (true)
+
   useEffect (() => {
     const fetchData = async () => {
   try {
-    const response = await fetch( "https://pokeapi.co/api/v2/pokemon/");
+    const response = await fetch( "https://dog.ceo/api/breeds/image/random");
     const jsonData = await response.json();
-    setData(jsonData.results);
+    setData(jsonData);
+    setCargando(false)
   } catch (error) {
     console.errror("error fetching data:",error);
   }
@@ -16,11 +20,22 @@ function App() {
 fetchData();
  }, []);
 
+const cambiar = ()=>setAbierto(!abierto)
+
 return(
   <>
+ 
+  {
+    cargando && <h1>ESPERE UNOS MINUTOS</h1>
+  }
 
-    { data.map((item)=><h1>{item.name}</h1> )}
+  {abierto &&
+  <div className='card'>
+    { <img src={data.message}></img> }
 
+  </div>
+
+  }
 
   </>
 ) 
