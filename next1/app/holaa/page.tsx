469e0911ea77de17c972b2ecbc304924f8d6971d
10tami <melinao10tami@gmail.com>
2025-10-16 21:22:37 -0300

    le añadi color al  texto

--- next1/app/holaa/page.tsx
+++ next1/app/holaa/page.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Link from "next/link"
+import Button from "@/app/componentes/boton"
 
 
 
 const page = () => {
   return (
-    <div>
-      hoolaaa
-      <h1>
-        recreooo
+    <div className='bg-cyan-500'>
+      HOLA!!!
+      <h1 className='bg-emerald-600'>
+        BIENVENID@!!!
       </h1>
+
     </div>
   )
 }
